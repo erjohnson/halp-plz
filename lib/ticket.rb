a8a6d6ef78247c3df01267ceba2b7435bd82f328
Eric <erjohnson.pdx@gmail.com>
2014-08-07 14:25:22 -0700

    Add ticket initialize

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,3 +1,12 @@
 class Ticket
+
+  def initialize attr
+    @partner_one = attr[:partner_one]
+    @partner_two = attr[:partner_two]
+    @subject = attr[:subject]
+    @issue = attr[:issue]
+    @time_stamp = attr[:time_stamp]
+    @done = attr[:done]
+  end
   
 end
--- spec/ticket_spec.rb
+++ spec/ticket_spec.rb
@@ -1,1 +1,11 @@
 require 'helper_spec'
+
+
+describe 'Ticket' do
+
+  it 'initializes with a hash' do
+    new_ticket = Ticket.new({ :partner_one=> 'Roger', :partner_two=> 'Kim' })
+    expect(new_ticket).to be_an_instance_of Ticket
+  end
+
+end
