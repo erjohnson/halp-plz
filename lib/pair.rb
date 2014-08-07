91d4fa1d425d28244d710937594bbc5f9c9a34f3
Eric <erjohnson.pdx@gmail.com>
2014-08-07 16:36:54 -0700

    Can read names of pair

--- lib/pair.rb
+++ lib/pair.rb
@@ -1,4 +1,6 @@
 class Pair
+
+  attr_reader :name_one, :name_two
   
   def initialize attributes
     @name_one = attributes[:name_one]
--- spec/pair_spec.rb
+++ spec/pair_spec.rb
@@ -7,4 +7,10 @@ describe 'Pair' do
       new_pair = Pair.new({})
     end
   end
+
+  describe 'object attributes' do
+    it 'lets you read pair names' do
+      new_pair = Pair.new({ :name_one=> 'Carol', :name_two=> 'Marty' })
+    end
+  end
 end
